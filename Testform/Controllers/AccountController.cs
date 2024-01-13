b0aa5e48e97acb5f814b0ffae338009436a4dc81
SotaChin <chin.sonita--3@gmail.com>
2024-01-13 16:38:16 +0700

    2 commit

---
+++ Testform/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testform.Controllers
+{
+    public class AccountController : Controller
+
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public  ActionResult Register(string name,string address,string phoneNumber,string password,string confirmPassword)
+        {
+            if(password==confirmPassword)
+            {
+                return Content("register success");
+            }
+            else
+            {
+                return Content("Password not match");
+            }
+        }
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ContentResult Validate(string uname, string pass)
+        {
+            // string user = Request["uname"];
+            // string pass = Request["pass"];
+            /* string user = value["uname"];
+              string pass = value["pass"];*/
+            //model binding primitive
+
+            if (uname == "admin" && pass == "123")
+            return Content("Welcome here");
+            else
+                return Content("Sorry Unavailable");
+            // return View();
+        }
+    }
+}
---
+++ Testform/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testform.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+       
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            int id = Convert.ToInt32( Request["id"]);
+            string myname = Request["name"];
+            string country = "";
+            switch(id)
+            {
+                case 1:
+                    country = "USA";
+                        break;
+                case 2:
+                    country = "China";
+                    break;
+                case 3:
+                    country = "Singapore";
+                    break;
+                default:
+                    country = "None";
+                    break;
+
+            }
+            return Content(myname+ " From " + country);
+           // return View();
+        }
+    }
+}
